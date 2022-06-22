158bb1bc8dc6f1e5c3d4bcde767d8b6432227026
Luana Neder <luananeder@gmail.com>
2022-06-22 20:25:20 -0300

    ok

--- kai.py
+++ kai.py
@@ -1,10 +1,7 @@
 import tweepy
 import os
 
-consumer_key = open("config/apikey", "r").read()
-consumer_secret = open("config/apisecret", "r").read()
-access_token = open("config/apitoken", "r").read()
-access_token_secret = open("config/tokensecret", "r").read()
+
 
 
 client = tweepy.Client(
@@ -12,5 +9,7 @@ client = tweepy.Client(
     access_token=access_token, access_token_secret=access_token_secret
 )
 
-tweet =  client.create_tweet(text="test")
-tweet2 = client.create_tweet(text="testing", in_reply_to_tweet_id=tweeet.id)
+tweet =  client.create_tweet(text="test1uu2a3123")
+print(tweet)
+
+tweet2 = client.create_tweet(text="testing", in_reply_to_tweet_id=tweet.data["id"])
