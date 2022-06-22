81e36b7dd6cbd6bebe1031bbcf9c752da5c4144f
Luana Neder <luananeder@gmail.com>
2022-06-22 20:12:30 -0300

    Update kai.py

--- kai.py
+++ kai.py
@@ -1,5 +1,16 @@
 import tweepy
 import os
 
-consumer_key = open("configs/apikey", "r")
+consumer_key = open("config/apikey", "r").read()
+consumer_secret = open("config/apisecret", "r").read()
+access_token = open("config/apitoken", "r").read()
+access_token_secret = open("config/tokensecret", "r").read()
 
+
+client = tweepy.Client(
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
+)
+
+tweet =  client.create_tweet(text="test")
+tweet2 = client.create_tweet(text="testing", in_reply_to_tweet_id=tweeet.id)
