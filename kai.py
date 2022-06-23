78f2ebba7013caa142deb94adda9db8d801c1889
Luana Neder <luananeder@gmail.com>
2022-06-23 16:34:43 -0300

    s

--- kai.py
+++ kai.py
@@ -2,6 +2,7 @@ import tweepy
 import os
 from textwrap import wrap
 
+#login info
 consumer_key = open("config/apikey", "r").read().replace('\n','')
 consumer_secret = open("config/apisecret", "r").read().replace('\n','')
 access_token = open("config/apitoken", "r").read().replace('\n','')
@@ -20,6 +21,7 @@ client = tweepy.Client(
 file = open("tweet", "r").read()
 txtsize = len(file)
 
+#get configs
 fstpre = open("config/firstpre", "r").read()
 fstpresize = len(fstpre)
 
@@ -40,7 +42,7 @@ tweetsize = 280 - presize - postsize
 print(tweetsize)
 
 lastsize = 280 - presize - lstpostsize
-
+print(lastsize)
 
 
 #fsttweet = wrap(file, fstsize, break_on_hyphens=False) # drop_whitespace=False does not work. how do I allow words to be broken in half?
@@ -48,9 +50,20 @@ lastsize = 280 - presize - lstpostsize
 #fsttweet = fsttweet[0]
 #print(fsttweet)
 
+#divide big text in tweets
 tweets = wrap(file, tweetsize, break_on_hyphens=False)
 twtnump = len(tweets)
 print(tweets)
+
+if len(tweets[twtnump - 1]) > lastsize:
+	print("last twt will have to be split")
+	alasttweets = wrap(tweets[twtnump - 1], lastsize)
+	number = twtnump + alasttweets - 1
+else:
+	print("last twt will not have to be split")
+	number = twtnump
+	
+print("there will be" + str(number) + "tweets")
 
 
 #twttxt = fstpre + fsttweet + post
@@ -60,9 +73,10 @@ for i in range(twtnump):
 		print("1st tweet now")
 		tweeti = tweets[i]
 		#twttxt = fstpre + fsttweet + post
-		twttxt = tweeti + post
+		posti = post.replace("%n", str(i + 1)).replace("%nu", str(i + 1)).replace("%T", str(number)).replace("%TO", str(number))
+		twttxt = tweeti + posti
 		tweet =  client.create_tweet(text=twttxt)
-		print("1st tweet done")
+		#print("1st tweet done")
 	elif i == len(tweets) - 1:
 		print("last tweet now")
 		tweeti = tweets[i]
@@ -74,23 +88,31 @@ for i in range(twtnump):
 				if i == len(lasttweets) - 1:
 					print("final last twt now")
 					tweeti = lasttweets[i]
-					twttxt = pre + tweeti + lastpost
+					prei = pre.replace("%n", str(i + 1)).replace("%nu", str(i + 1)).replace("%T", str(number)).replace("%TO", str(number))
+					lastposti = lastpost.replace("%n", str(i + 1)).replace("%nu", str(i + 1)).replace("%T", str(number)).replace("%TO", str(number))
+					twttxt = prei + tweeti + lastposti
 					tweet = client.create_tweet(text=twttxt, in_reply_to_tweet_id=tweet.data["id"])
 				else:
-					print("lst twt part" + str(i) + "+1 now")
+					print("lst twt part " + str(i) + "+1 now")
 					tweeti = lasttweets[i]
-					twttxt = pre + tweeti + post
+					prei = pre.replace("%n", str(i + 1)).replace("%nu", str(i + 1)).replace("%T", str(number)).replace("%TO", str(number))
+					posti = post.replace("%n", str(i + 1)).replace("%nu", str(i + 1)).replace("%T", str(number)).replace("%TO", str(number))
+					twttxt = prei + tweeti + posti
 					tweet = client.create_tweet(text=twttxt, in_reply_to_tweet_id=tweet.data["id"])
 		else:
 			print("no need to split")
-			twttxt = pre + tweeti + lastpost
+			prei = pre.replace("%n", str(i + 1)).replace("%nu", str(i + 1)).replace("%T", str(number)).replace("%TO", str(number))
+			lastposti = lastpost.replace("%n", str(i + 1)).replace("%nu", str(i + 1)).replace("%T", str(number)).replace("%TO", str(number))
+			twttxt = prei + tweeti + lastposti
 			tweet = client.create_tweet(text=twttxt, in_reply_to_tweet_id=tweet.data["id"])
 	else:
 		print("tweet " + str(i) + "+1 now")
 		tweeti = tweets[i]
-		twttxt = pre + tweeti + post
+		prei = pre.replace("%n", str(i + 1)).replace("%nu", str(i + 1)).replace("%T", str(number)).replace("%TO", str(number))
+		posti = post.replace("%n", str(i + 1)).replace("%nu", str(i + 1)).replace("%T", str(number)).replace("%TO", str(number))
+		twttxt = prei + tweeti + posti
 		tweet = client.create_tweet(text=twttxt, in_reply_to_tweet_id=tweet.data["id"])
-		print("tweet " + str(i) + "+1 done")
+		#print("tweet " + str(i) + "+1 done")
 
 
 #tweet =  client.create_tweet(text="hello")
